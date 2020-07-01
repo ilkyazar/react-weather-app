27467690ce86c53b8f2a79cec363e205601c356c
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-07-01 16:50:36 +0300

    fix mixed content

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends React.Component {
 
         if (city && country) {
             const api_call = await fetch(
-                `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${constants.API_KEY}`
+                `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${constants.API_KEY}`
                 );
     
             const response = await api_call.json();
